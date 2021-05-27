3043bade6eb5b49a43f95488541e70b3872f9da8
Furzy <t.furzy@gmail.com>
2021-05-27 13:07:14 +0200

    Trying new workflow using animationEvents

--- Assets/Scripts/Takuma_AnimationScript.cs
+++ Assets/Scripts/Takuma_AnimationScript.cs
@@ -5,19 +5,15 @@ using UnityEngine;
 public class Takuma_AnimationScript : MonoBehaviour
 {
     public AnimationClip currentClip;
-    public float total_frames_in_animation;
-    public float desired_frame;
     public Animator animator;
 
     private void Awake() => animator = GetComponent<Animator>();
 
-    public void DoLoop(string animationName)
+    public void GoToFrame(float desiredFrame)
     {
-        currentClip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
-        total_frames_in_animation = currentClip.length;
-        Debug.log(total_frames_in_animation.ToString());
-        desired_frame = 43f;
-        animator.Play(animationName, 0, ( 1f / total_frames_in_animation ) * desired_frame);
-
+        var currentClip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
+        var animationName = currentClip.name;
+        var nbFramesInClip = currentClip.length * 60f;
+        animator.Play(animationName, 0, ( 1f / nbFramesInClip ) * desiredFrame);
     }
 }
