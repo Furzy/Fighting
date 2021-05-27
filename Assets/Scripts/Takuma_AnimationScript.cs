4bef92ec1ead7b0ca941e461ee956161200856a6
Furzy <t.furzy@gmail.com>
2021-05-27 16:48:28 +0200

    Added CROUCH animation + loop

--- Assets/Scripts/Takuma_AnimationScript.cs
+++ Assets/Scripts/Takuma_AnimationScript.cs
@@ -9,11 +9,18 @@ public class Takuma_AnimationScript : MonoBehaviour
 
     private void Awake() => animator = GetComponent<Animator>();
 
+    private void Update() {
+        if (Input.GetKey(KeyCode.DownArrow) && !Input.GetKeyUp(KeyCode.DownArrow))
+            animator.Play("Takuma_CROUCH", 0);
+        else if (!Input.GetKey(KeyCode.DownArrow) && Input.GetKeyUp(KeyCode.DownArrow))
+            animator.Play("Takuma_IDLE", 0);
+    }
+
     private void GoToFrame(float desiredFrame)
     {
-        var currentClip = animator.runtimeAnimatorController.animationClips[0];
-        var animationName = currentClip.name;
+        var currentClip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
+        Debug.Log(currentClip.name);
         var nbFramesInClip = currentClip.length * 60f;
-        animator.Play(animationName, 0, ( 1f / nbFramesInClip ) * desiredFrame);
+        animator.Play(currentClip.name, 0, ( 1f / nbFramesInClip ) * desiredFrame);
     }
 }
